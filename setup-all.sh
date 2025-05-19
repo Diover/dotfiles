76838526ad74a756aa86c090ed5f19391734cc79
Diover <istatishere@gmail.com>
2025-05-19 16:22:27 -0700

    Add fd to the setup script. The utility to do file search with Telescope in nvim.

--- setup-all.sh
+++ setup-all.sh
@@ -1,5 +1,5 @@
 brew install --cask nikitabobko/tap/aerospace;
 brew install --cask font-jetbrains-mono-nerd-font;
-brew install starship neovim fzf zsh-autosuggestions zsh-syntax-highlighting;
+brew install starship neovim fzf fd zsh-autosuggestions zsh-syntax-highlighting;
 
 #brew install --cask miniconda;
