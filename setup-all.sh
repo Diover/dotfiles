462d286646ae1f41e2b7017e8f5bdac14531ab47
Diover <istatishere@gmail.com>
2025-05-19 15:18:13 -0700

    Add nerd font install

--- setup-all.sh
+++ setup-all.sh
@@ -1,4 +1,5 @@
 brew install --cask nikitabobko/tap/aerospace;
+brew install --cask font-jetbrains-mono-nerd-font;
 brew install starship neovim fzf zsh-autosuggestions zsh-syntax-highlighting;
 
 #brew install --cask miniconda;
