d8ae1584aa19336465b774c12683bd3771fe68be
Diover <istatishere@gmail.com>
2025-06-13 00:21:40 -0700

    Add meslo nerd font

--- install-all.sh
+++ install-all.sh
@@ -1,4 +1,4 @@
 brew install --cask nikitabobko/tap/aerospace
-brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask font-jetbrains-mono-nerd-font font-meslo-lg-nerd-font
 brew install starship neovim fzf fd zsh-autosuggestions zsh-syntax-highlighting lua-language-server yazi coreutils reattach-to-user-namespace
 #brew install --cask miniconda;
