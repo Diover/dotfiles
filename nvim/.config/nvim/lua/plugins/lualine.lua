98c9d292f85cf622a793f0f34ef2c6a549d3faf8
Diover <istatishere@gmail.com>
2025-06-17 00:19:05 -0700

    Change lualine settings to include relative file path and add more flavor

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,35 +1,36 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "echasnovski/mini.icons" },
-  config = function()
-    local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "echasnovski/mini.icons" },
+	config = function()
+		local lualine = require("lualine")
 
-    -- configure lualine with modified theme
-    lualine.setup({
-      options = {
-        icons_enabled = false,
-        theme = "auto",
-        component_separators = "",
-        section_separators = "",
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = { "filename" },
-        lualine_x = {
-          function()
-            local encoding = vim.o.fileencoding
-            if encoding == "" then
-              return vim.bo.fileformat .. " :: " .. vim.bo.filetype
-            else
-              return encoding .. " :: " .. vim.bo.fileformat .. " :: " .. vim.bo.filetype
-            end
-          end,
-        },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-    })
-  end,
+		-- configure lualine with modified theme
+		lualine.setup({
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				globalstatus = true,
+				always_divide_middle = true,
+			},
+
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				-- Full path
+				lualine_c = { { "filename", path = 1 } },
+				lualine_x = {
+					function()
+						local encoding = vim.o.fileencoding
+						if encoding == "" then
+							return vim.bo.fileformat .. " :: " .. vim.bo.filetype
+						else
+							return encoding .. " :: " .. vim.bo.fileformat .. " :: " .. vim.bo.filetype
+						end
+					end,
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
