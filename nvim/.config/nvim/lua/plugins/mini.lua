36d2151e0971f8fe0cbe80e5de07a721ffd3d949
Diover <istatishere@gmail.com>
2025-06-21 22:18:27 -0700

    Refine lspconfig keymaps. Make pairing better in mini.pairs

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -9,7 +9,45 @@ return {
 			--  - va)  - [V]isually select [A]round [)]paren
 			--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
 			--  - ci'  - [C]hange [I]nside [']quote
-			require("mini.ai").setup({ n_lines = 500 })
+			local ai = require("mini.ai")
+			ai.setup({
+				custom_textobjects = {
+					a = ai.gen_spec.treesitter({ a = "@parameter.outer", i = "@parameter.inner" }),
+					c = ai.gen_spec.treesitter({ a = "@comment.outer", i = "@comment.inner" }),
+					s = { -- Single words in different cases (camelCase, snake_case, etc.)
+						{
+							"%u[%l%d]+%f[^%l%d]",
+							"%f[^%s%p][%l%d]+%f[^%l%d]",
+							"^[%l%d]+%f[^%l%d]",
+							"%f[^%s%p][%a%d]+%f[^%a%d]",
+							"^[%a%d]+%f[^%a%d]",
+						},
+						"^().*()$",
+					},
+					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+					g = function() -- whole file
+						local from = { line = 1, col = 1 }
+						local to = {
+							line = vim.fn.line("$"),
+							col = math.max(vim.fn.getline("$"):len(), 1),
+						}
+						return { from = from, to = to }
+					end,
+				},
+				mappings = {
+					around = "a",
+					inside = "i",
+
+					around_next = "an",
+					inside_next = "in",
+					around_last = "ap",
+					inside_last = "ip",
+
+					goto_left = "g[",
+					goto_right = "g]",
+				},
+				n_lines = 500,
+			})
 		end,
 	},
 	{
@@ -26,7 +64,6 @@ return {
 			require("mini.pairs").setup({
 				-- In which modes mappings from this `config` should be created
 				modes = { insert = true, command = false, terminal = false },
-
 				-- Global mappings. Each right hand side should be a pair information, a
 				-- table with at least these fields (see more in |MiniPairs.map|):
 				-- - <action> - one of 'open', 'close', 'closeopen'.
@@ -35,17 +72,70 @@ return {
 				-- `<CR>`, `'` does not insert pair after a letter.
 				-- Only parts of tables can be tweaked (others will use these defaults).
 				mappings = {
-					["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
-					["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
-					["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
-
+					["["] = {
+						action = "open",
+						pair = "[]",
+						neigh_pattern = ".[%s%z%)}%]]",
+						register = { cr = false },
+						-- foo|bar -> press "[" -> foo[bar
+						-- foobar| -> press "[" -> foobar[]
+						-- |foobar -> press "[" -> [foobar
+						-- | foobar -> press "[" -> [] foobar
+						-- foobar | -> press "[" -> foobar []
+						-- {|} -> press "[" -> {[]}
+						-- (|) -> press "[" -> ([])
+						-- [|] -> press "[" -> [[]]
+					},
+					["{"] = {
+						action = "open",
+						pair = "{}",
+						-- neigh_pattern = ".[%s%z%)}]",
+						neigh_pattern = ".[%s%z%)}%]]",
+						register = { cr = false },
+						-- foo|bar -> press "{" -> foo{bar
+						-- foobar| -> press "{" -> foobar{}
+						-- |foobar -> press "{" -> {foobar
+						-- | foobar -> press "{" -> {} foobar
+						-- foobar | -> press "{" -> foobar {}
+						-- (|) -> press "{" -> ({})
+						-- {|} -> press "{" -> {{}}
+					},
+					["("] = {
+						action = "open",
+						pair = "()",
+						-- neigh_pattern = ".[%s%z]",
+						neigh_pattern = ".[%s%z%)]",
+						register = { cr = false },
+						-- foo|bar -> press "(" -> foo(bar
+						-- foobar| -> press "(" -> foobar()
+						-- |foobar -> press "(" -> (foobar
+						-- | foobar -> press "(" -> () foobar
+						-- foobar | -> press "(" -> foobar ()
+					},
 					[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
 					["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
 					["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
-
-					['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
-					["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
-					["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
+					-- Single quote: Prevent pairing if either side is a letter
+					['"'] = {
+						action = "closeopen",
+						pair = '""',
+						neigh_pattern = "[^%w\\][^%w]",
+						register = { cr = false },
+					},
+					-- Single quote: Prevent pairing if either side is a letter
+					["'"] = {
+						action = "closeopen",
+						pair = "''",
+						neigh_pattern = "[^%w\\][^%w]",
+						register = { cr = false },
+					},
+					-- Backtick: Prevent pairing if either side is a letter
+					["`"] = {
+						action = "closeopen",
+						pair = "``",
+						neigh_pattern = "[^%w\\][^%w]",
+						register = { cr = false },
+					},
 				},
 			})
 		end,
