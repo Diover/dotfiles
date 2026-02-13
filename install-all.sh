dbaf1a3786c662ec0acf5b52b10b935f027cda1a
Diover <istatishere@gmail.com>
2026-02-12 22:37:14 -0800

    Add gitlab plugin

--- install-all.sh
+++ install-all.sh
@@ -2,4 +2,5 @@
 brew install --cask nikitabobko/tap/aerospace
 brew install --cask font-jetbrains-mono-nerd-font font-meslo-lg-nerd-font
 brew install starship neovim fzf fd zsh-autosuggestions zsh-syntax-highlighting lua-language-server yazi coreutils reattach-to-user-namespace ripgrep ast-grep obsidian pngpaste
+brew install go
 #brew install --cask miniconda;
---
+++ nvim/.config/nvim/lua/plugins/gitlab.lua
@@ -0,0 +1,34 @@
+return {
+	"harrisoncramer/gitlab.nvim",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"nvim-lua/plenary.nvim",
+		"sindrets/diffview.nvim",
+		"nvim-tree/nvim-web-devicons", -- Recommended but not required. Icons in discussion tree.
+	},
+	build = function()
+		require("gitlab.server").build(true)
+	end, -- Builds the Go binary
+	config = function()
+		local gitlab_auth_token = "unknown_token"
+		local gitlab_url = "unknown_url"
+		local gitlab_config_path = vim.fn.expand("$HOME/.gitlab.nvim")
+
+		if vim.fn.filereadable(gitlab_config_path) == 1 then
+			for line in io.lines(gitlab_config_path) do
+				local splits = vim.split(line, "=")
+				if splits[1] == "auth_token" then
+					gitlab_auth_token = splits[2]
+				end
+				if splits[1] == "gitlab_url" then
+					gitlab_url = splits[2]
+				end
+			end
+		end
+		require("gitlab").setup({
+			auth_provider = function()
+				return gitlab_auth_token, gitlab_url, nil
+			end,
+		})
+	end,
+}
