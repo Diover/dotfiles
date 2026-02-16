9fc551f58d6baab36f7a4e9e5617de7a241b8184
Diover <istatishere@gmail.com>
2026-02-15 23:25:49 -0800

    Add context-aware indent selection (tresitter-based).

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,11 +1,24 @@
+-- Uncomment after (this)[https://github.com/lukas-reineke/indent-blankline.nvim/issues/649] is implemented
+-- return {
+-- 	"lukas-reineke/indent-blankline.nvim",
+-- 	main = "ibl",
+-- 	---@module "ibl"
+-- 	---@type ibl.config
+-- 	opts = {
+-- 		indent = {
+-- 			char = "▏",
+-- 		},
+-- 	},
+-- }
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	main = "ibl",
-	---@module "ibl"
-	---@type ibl.config
-	opts = {
-		indent = {
-			char = "▏",
-		},
-	},
+	"kiyoon/indent-blankline-v2.nvim",
+	event = "BufReadPost",
+	config = function()
+		vim.opt.list = true
+		require("indent_blankline").setup({
+			space_char_blankline = " ",
+			show_current_context = true,
+			show_current_context_start = true,
+		})
+	end,
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -138,4 +138,43 @@ return {
 		dependencies = { "nvim-treesitter" },
 		opts = {},
 	},
+	{
+		"kiyoon/treesitter-indent-object.nvim",
+		dependencies = { "nvim-treesitter" },
+		keys = {
+			{
+				"ai",
+				function()
+					require("treesitter_indent_object.textobj").select_indent_outer()
+				end,
+				mode = { "x", "o" },
+				desc = "Select context-aware indent (outer)",
+			},
+			{
+				"aI",
+				function()
+					require("treesitter_indent_object.textobj").select_indent_outer(true, "V")
+					require("treesitter_indent_object.refiner").include_surrounding_empty_lines()
+				end,
+				mode = { "x", "o" },
+				desc = "Select context-aware indent (outer, line-wise)",
+			},
+			{
+				"ii",
+				function()
+					require("treesitter_indent_object.textobj").select_indent_inner()
+				end,
+				mode = { "x", "o" },
+				desc = "Select context-aware indent (inner, partial range)",
+			},
+			{
+				"iI",
+				function()
+					require("treesitter_indent_object.textobj").select_indent_inner(true, "V")
+				end,
+				mode = { "x", "o" },
+				desc = "Select context-aware indent (inner, entire range) in line-wise visual mode",
+			},
+		},
+	},
 }
