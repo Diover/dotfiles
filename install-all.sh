7461f26366fefdde39bc8e609cab0b5903918496
Diover <istatishere@gmail.com>
2026-03-23 23:13:14 -0700

    Refactor installation script

--- install-all.sh
+++ install-all.sh
@@ -1,7 +1,6 @@
 #!/bin/zsh
 brew install --cask nikitabobko/tap/aerospace
-brew install --cask font-jetbrains-mono-nerd-font font-meslo-lg-nerd-font font-zed-mono-nerd-font font-iosevka-term-nerd-font
- brew install --cask font-zed-mono-nerd-font
+brew install --cask font-jetbrains-mono-nerd-font font-meslo-lg-nerd-font font-zed-mono-nerd-font font-iosevka-term-nerd-font font-zed-mono-nerd-font
 brew install starship neovim fzf fd zsh-autosuggestions zsh-syntax-highlighting lua-language-server yazi coreutils reattach-to-user-namespace ripgrep ast-grep obsidian pngpaste imagemagick
 brew install --cask kitty
 brew install go
